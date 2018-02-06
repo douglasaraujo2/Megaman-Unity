c2747d9759e1c07b4cdf9a52c62eee45890dad8c
logonrm <logonrm@fiap.com.br>
2018-02-05 22:29:25 -0200

    Project commit

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour {
+	public float intervalo;
+	IEnumerator Start () {
+		GetComponent<SpriteRenderer> ().enabled = false;
+		yield return new WaitForSeconds (intervalo);
+		GetComponent<SpriteRenderer> ().enabled = true;
+		yield return new WaitForSeconds (intervalo);
+		StartCoroutine (Start ());
+	}
+
+}
